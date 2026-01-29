6c711a60aeec0a62a679e1131a3ac7e326630607
Sanjay Chandrakant Ghadigaonkar <sanjay07.scg@gmail.com>
2026-01-29 17:02:30 +0530

    Day 01 completed

---
+++ day1/javascript.js
@@ -0,0 +1,34 @@
+
+
+
+let Income = document.getElementById("salaryInput");
+let CalcBtn  = document.getElementById("calcBtn");
+let TaxPrice = document.getElementById("taxPrice");
+let FinalSalary = document.getElementById("finalSalary");
+
+     CalcBtn.addEventListener("click" , function() {
+      
+
+    //  console.log("btnclick");
+      let salary = Number(Income.value);
+
+      if (salary === 0 || salary === "") {
+        alert("First enter income!")
+        return;
+      }
+
+       let tax = 0;
+
+      if (salary > 1000000 ) {
+         tax = salary * 0.10; 
+        }else if (salary > 500000 ) {
+         tax = salary * 0.15; 
+     } else {
+      tax = 0 ; //no tax
+      
+     }
+       let bachaHuaaPaisa = salary - tax ;
+       TaxPrice.innerText = `YourTax  ${tax}`;
+       FinalSalary.innerText = `YourMoneyLeft ${bachaHuaaPaisa}`;
+
+     });
